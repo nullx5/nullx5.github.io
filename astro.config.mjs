81ce0a04cbbac8f2d6ffcccbe7ed43d81ef2b3f8
bl3ssedc0de <john@example.com>
2025-03-11 03:27:45 -0500

    Change dominio /blog - astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nullx5.github.io',
+	site: 'https://nullx5.github.io/blog/',
 	integrations: [mdx(), sitemap()],
 });
